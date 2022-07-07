c05d09c6d18661a67687c7c8f1d45a78900a7b61
Omar Ashraf <omar.ashraf.zeinhom@Hotmail.com>
2022-07-07 12:28:59 +0200

    added-better-ui

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,133 +1,100 @@
 import {
   IonButton,
   IonCol,
+  IonContent,
   IonGrid,
   IonHeader,
   IonIcon,
   IonInput,
   IonItem,
+  IonItemDivider,
   IonPage,
   IonRow,
   IonText,
   IonTitle,
+  IonToolbar,
 } from "@ionic/react";
 import React, { useEffect } from "react";
 import "./Search.css";
 import { useState } from "react";
-import { search } from "ionicons/icons";
-import { url } from "inspector";
+import { search, book } from "ionicons/icons";
 
 const Search = () => {
+  const [userinput, setUserinput] = useState([]);
+  //
+  const [datasearched, setDatasearched] = useState([]);
 
-const [ userinput, setUserinput ] = useState([]);
-//
-const [datasearched, setDatasearched] = useState([]);
+  useEffect(() => {
+    //debug userinput
+    console.log(userinput);
+  });
 
+  //debug
+  //console.log(setUserinput);
 
+  const searchApi = () => {
+    postSearchedAPIData().then((data) => {
+      setDatasearched(data);
 
-
-
-useEffect(()=>{
-  //debug userinput
-  console.log(userinput);
-})
-
-
-
-
-
-//debug
-//console.log(setUserinput);
-
-
-
-
-
-
-
-const searchApi= () =>{
-postSearchedAPIData().then((data)=>{
-  setDatasearched(data);
-
-const stringifiedData = JSON.stringify(data);
-console.log(stringifiedData);
-
-})
+      const stringifiedData = JSON.stringify(data);
+      console.log(stringifiedData);
+    });
     console.log("api searched");
-  
-  
-  
-  
-  }
+  };
 
-  
-const stringDataDisplay = JSON.stringify([datasearched]);
-
-
+  const stringDataDisplay = JSON.stringify([datasearched]);
 
   /** Button Search  */
-  
-  
-  //1 async arrow function do a fetch request or a get request from the api
-  //2 await 
-  //3 Pass the object from api as Stringied JSON to show the results 
 
-  // 4 Add function to onclick in button 
+  //1 async arrow function do a fetch request or a get request from the api
+  //2 await
+  //3 Pass the object from api as Stringied JSON to show the results
+
+  // 4 Add function to onclick in button
   //searchapi [x]
   //display the data []
 
-
-
-
-
-
-
-async function postSearchedAPIData(
-  url = `https://api.nationalize.io?name=${userinput}`
- ) {
-  const response = await fetch(url, {
-    method: "GET",
-  });
-  return response.json();
-}
-
-
-
-
-
-
+  async function postSearchedAPIData(
+    url = `https://api.alquran.cloud/v1/search/${userinput}/all/en`
+  ) {
+    const response = await fetch(url, {
+      method: "GET",
+    });
+    return response.json();
+  }
 
   return (
     <IonPage>
-      <IonHeader>
-        <IonTitle>Search Page</IonTitle>
-      </IonHeader>
+      <IonContent>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Quran API Search Page</IonTitle>
+          </IonToolbar>
+        </IonHeader>
 
-      <IonGrid>
-        <IonRow>
-          <IonCol size="12">
-            <IonItem>
-              <IonInput 
-              placeholder="Search Api here" 
-              onIonChange={(event:any)=> setUserinput(event.target.value!)}
-              />
-              <IonIcon icon={search} />
-            
-            <IonButton
-            onClick={searchApi}
-            >
-              Search
-            </IonButton>
-            </IonItem>
-<IonText>
-  {[stringDataDisplay]}
-</IonText>
+        <IonGrid>
+          <IonRow>
+            <IonCol size="12" class="ion-padding">
+              <IonItem>
+                <IonIcon icon={book} class="ion-center" />
+                <IonInput
+                  placeholder="Search Api here"
+                  onIonChange={(event: any) =>
+                    setUserinput(event.target.value!)
+                  }
+                />
+                <IonIcon icon={search} />
 
-
-
-          </IonCol>
-        </IonRow>
-      </IonGrid>
+                <IonButton onClick={searchApi}>Search</IonButton>
+              </IonItem>
+              <IonItemDivider />
+            </IonCol>
+            <IonCol size="12" class="ion-padding">
+              <IonText class="ion-text-center">{[stringDataDisplay]}</IonText>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
     </IonPage>
   );
 };
